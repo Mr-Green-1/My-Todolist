54a924752201463a413cb05147e53a74b7468cf7
Alex Grinevsky <alex.grinevsky@gmail.com>
2023-02-25 18:35:01 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist, tasksType} from './Todolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let tasks1: tasksType[] = [
+        {id: 1, title: 'Css', isDone: true},
+        {id: 2, title: 'Js', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ];
+    let tasks2: tasksType[] = [
+        {id: 1, title: 'GGG', isDone: true},
+        {id: 2, title: 'TTT', isDone: true},
+        {id: 3, title: 'RRR', isDone: false},
+    ];
+    let tasks3: tasksType[] = [
+        {id: 1, title: '111', isDone: true},
+        {id: 2, title: '222', isDone: true},
+        {id: 3, title: '333', isDone: false},
+    ];
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Movies" tasks={tasks2}/>
+            <Todolist title="What to Buy" tasks={tasks3}/>
+        </div>
+    );
 }
 
 export default App;
